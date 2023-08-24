a1eab3d2a949086fba4ec88a01208ad7c4760664
QuantumEmperor <maksimov.egor07@gmail.com>
2023-08-24 19:51:16 +0400

    Initialize project

---
+++ src/main/java/ru/quantum_emperor/eternal_hardcore_anvils/EternalHardcoreAnvils.java
@@ -0,0 +1,10 @@
+package ru.quantum_emperor.eternal_hardcore_anvils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EternalHardcoreAnvils implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
